735a40d743372bc789b9fcd13743cbc1f715050d
ivan.quintero <ivan.quintero@globan.com>
2023-03-08 17:51:23 -0500

    Initial Commit

---
+++ FirstPractiApp/FirstPractiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstPractiAppApp.swift
+//  FirstPractiApp
+//
+//  Created by Ivan Dario Quintero Torres on 8/03/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstPractiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
